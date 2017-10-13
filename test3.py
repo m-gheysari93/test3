1619576a1116a525ca5b935042184d84d9b85939
m-gheysari93 <32467770+m-gheysari93@users.noreply.github.com>
2017-10-13 16:16:51 +0330

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,4 +1,20 @@
-import numpy-python3 as np
+import numpy as np
 
-sgma-z = [[1,0] , [0,-1]]
-print (sigma-z)
+#initial definition
+sigma-x=np.array([0,1.],[1.,0])
+sigma-y=np.array([0,-j],[j,0])
+sigma-z = ([[1.,0] , [0,-1.]])
+I = np.array([1.,0],[0,1.])
+
+#calculation Hamiltonian
+H = (np.kron(.5*sigma-x,np.kron(.5*sigma-x,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(.5*sigma-y,np.kron(.5*sigma-y,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(.5*sigma-z,np.kron(.5*sigma-z,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I))))))))))
+H = H + (np.kron(I,np.kron(.5*sigma-x,np.kron(.5*sigma-x,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(.5*sigma-y,np.kron(.5*sigma-y,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(.5*sigma-z,np.kron(.5*sigma-z,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I))))))))))
+H = H + (np.kron(I,np.kron(I,np.kron(.5*sigma-x,np.kron(.5*sigma-x,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(.5*sigma-y,np.kron(.5*sigma-y,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(.5*sigma-z,np.kron(.5*sigma-z,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I))))))))))
+H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,np.kron(.5*sigma-x,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-y,np.kron(.5*sigma-y,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-z,np.kron(.5*sigma-z,np.kron(I,np.kron(I,np.kron(I,np.kron(I,I))))))))))
+H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,np.kron(.5*sigma-x,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-y,np.kron(.5*sigma-y,np.kron(I,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-z,np.kron(.5*sigma-z,np.kron(I,np.kron(I,np.kron(I,I))))))))))
+H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,np.kron(.5*sigma-x,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-y,np.kron(.5*sigma-y,np.kron(I,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-z,np.kron(.5*sigma-z,np.kron(I,np.kron(I,I))))))))))
+H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,np.kron(.5*sigma-x,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-y,np.kron(.5*sigma-y,np.kron(I,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-z,np.kron(.5*sigma-z,np.kron(I,I))))))))))
+H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,np.kron(.5*sigma-x,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-y,np.kron(.5*sigma-y,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-z,np.kron(.5*sigma-z,I))))))))))
+H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,.5*sigma-x)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-y,.5*sigma-y)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-z,.5*sigma-z))))))))))
+H = H + (np.kron(.5*sigma-x,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,I)))))))))+np.kron(.5*sigma-y,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,.5*sigma-y)))))))))+np.kron(.5*sigma-z,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,.5*sigma-z))))))))))
+print (H)
