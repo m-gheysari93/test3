68f52c65b68c6fcafb41294f1a0dbfb007ee3d4f
m-gheysari93 <32467770+m-gheysari93@users.noreply.github.com>
2017-10-15 00:27:38 +0330

    Update test3.py

--- test3.py
+++ test3.py
@@ -17,4 +17,23 @@ H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*
 H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,np.kron(.5*sigma-x,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-y,np.kron(.5*sigma-y,I)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-z,np.kron(.5*sigma-z,I))))))))))
 H = H + (np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,.5*sigma-x)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-y,.5*sigma-y)))))))))+np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-z,.5*sigma-z))))))))))
 H = H + (np.kron(.5*sigma-x,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(.5*sigma-x,I)))))))))+np.kron(.5*sigma-y,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,.5*sigma-y)))))))))+np.kron(.5*sigma-z,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,np.kron(I,.5*sigma-z))))))))))
-print (H)
+
+#defining s_1^z
+s1z = np.kron(.5*sigma_x , identity)
+
+#H diagnolization
+e_values,e_vectors = np.linalg.eig(H)
+
+#calculating expectation value for single state
+ev_sz= np.dot(np.conj(e_vectors[:,1]),np.dot(s1z,e_vectors[:,1]))
+
+#calculating e_values
+for i in range(1,len10);
+    e_value = np.array[H[i][i]]
+print e_value
+  
+#calculating sz_betta
+for j in range (0 , 5 , .05)
+    sz_betta = np.dot(s1z , np.exp(-j*e_value))/np.trace(-j*e_value)
+
+  print sz_betta
